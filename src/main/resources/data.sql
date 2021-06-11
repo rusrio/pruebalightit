f78185624dd309956e0eff185d30a4c1659cfa95
rusrio <77778150+rusrio@users.noreply.github.com>
2021-06-11 10:23:45 +0200

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
 INSERT INTO `iiq5rhkiaa0xhl9k`.`roles` (`nombre`) VALUES ('ROLE_ADMIN');
-INSERT INTO `iiq5rhkiaa0xhl9k`.`roles` (`nombre`) VALUES ('ROLE_USUARIO');
+INSERT INTO `iiq5rhkiaa0xhl9k`.`roles` (`nombre`) VALUES ('ROLE_REGISTRADO');
