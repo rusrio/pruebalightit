2cb6b1771e775307a9d5555a2742d68ba1f9e57e
Ruslan Rio <ruslanrm29@educastur.es>
2021-06-13 19:09:06 +0200

    arreglos finales

--- src/main/java/com/dawes/controller/ProductoController.java
+++ src/main/java/com/dawes/controller/ProductoController.java
@@ -5,6 +5,7 @@ import java.security.Principal;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,6 +32,7 @@ public class ProductoController {
 		return "tienda";
 	}
 	
+	@PreAuthorize("hasRole('ROLE_ADMINISTRADOR')")
 	@GetMapping("/gestion_productos")
 	public String gestion_productos(Model modelo) {
 	modelo.addAttribute("productos", sp.findAll());
@@ -44,6 +46,7 @@ public class ProductoController {
 		return "adminhtml/insertarProducto";
 	}
 	
+	@PreAuthorize("hasRole('ROLE_ADMINISTRADOR')")
 	@PostMapping("/submitProducto")
 	public String submit(@ModelAttribute ProductoVO producto,Model modelo) {
 		sp.save(producto);
@@ -51,6 +54,7 @@ public class ProductoController {
 		return "adminhtml/gestion_productos";
 	}
 	
+	@PreAuthorize("hasRole('ROLE_ADMINISTRADOR')")
 	@GetMapping("/eliminarProducto")
 	public String eliminar(@RequestParam int idproducto, Model modelo){
 		sp.deleteById(idproducto);
@@ -58,6 +62,7 @@ public class ProductoController {
 		return "adminhtml/gestion_productos";
 	}
 	
+	@PreAuthorize("hasRole('ROLE_ADMINISTRADOR')")
 	@GetMapping("/modificarProducto")
 	public String modificar(@RequestParam int idproducto, Model modelo){
 		ProductoVO pr=sp.findById(idproducto).get();
